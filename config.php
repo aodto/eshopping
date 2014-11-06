fe55eaf3b68dfa555a09a84e4cd9446edfc3aef0
aodto <free_flash_design@hotmail.com>
2014-11-06 09:32:23 +0700

    update config file

--- config.php
+++ config.php
@@ -1,10 +1,10 @@
 <?php
 $config = array();
 
-$config['base_url'] = "http://localhost/";
+$config['base_url'] = "http://localhost/eshopping/";
 
 $config['database']['host'] = "localhost";
-$config['database']['port'] = "27017";
+$config['database']['port'] = 3306;
 $config['database']['username'] = "root";
 $config['database']['password'] = "";
 $config['database']['dbname'] = "eshopping";
